aa1360b54c0b60faf2f2216dfecd100488aa7fe3
Test126k <test126126k@gmail.com>
2025-02-20 00:46:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,19 +11,19 @@ from logging.handlers import RotatingFileHandler
 load_dotenv("config.env")
 
 # Bot token dari @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7646699128:AAE6uoyjQYhml8fnMpuTeeLo5kGFlp_w5l4")
 
 # API ID Anda dari my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", "25649636"))
+APP_ID = int(os.environ.get("APP_ID", "26300022"))
 
 # API Hash Anda dari my.telegram.org
-API_HASH = os.environ.get("API_HASH", "43af470d1c625e603733268b3c2f7b8f")
+API_HASH = os.environ.get("API_HASH", "def44e13defba9d104323e821955dfa3")
 
 # ID Channel Database
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "0"))
 
 # Protect Content
-PROTECT_CONTENT = strtobool(os.environ.get("PROTECT_CONTENT", "False"))
+PROTECT_CONTENT = strtobool(os.environ.get("PROTECT_CONTENT", "True"))
 
 # Heroku Credentials for updater.
 HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
@@ -36,11 +36,11 @@ UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 DB_URI = os.environ.get("DATABASE_URL", "")
 
 #Database MONGO
-MONGO_URI = os.environ.get("MONGO_URI", "")
-MONGO_NAME = os.environ.get("MONGO_NAME", "")
+MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://Master2:Ak997110@cluster0.9qw9v.mongodb.net/?retryWrites=true&w=majority")
+MONGO_NAME = os.environ.get("MONGO_NAME", "clister0")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0")) 
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001913183764")) 
 FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
@@ -67,7 +67,7 @@ CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 # Setel True jika Anda ingin Menonaktifkan tombol Bagikan Kiriman Saluran Anda
 DISABLE_CHANNEL_BUTTON = strtobool(os.environ.get("DISABLE_CHANNEL_BUTTON", "False"))
 
-ADMINS.append(5333099884) 
+ADMINS.append(6057768840) 
 
 
 LOG_FILE_NAME = "logs.txt"
